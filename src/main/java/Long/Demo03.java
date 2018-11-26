1425fd6982831daefe0c807fd5937006a5d6071b
zhuguolong <zhuguolongi@outlook.com>
2018-11-26 15:00:19 +0800

    String transform Long

---
+++ src/main/java/Long/Demo03.java
@@ -0,0 +1,14 @@
+package Long;
+
+/**
+ * @author zhugu
+ * 2018/11/26 14:59
+ */
+public class Demo03 {
+    public static void main(String[] args) {
+        String str = "1";
+
+        long l = Long.parseLong(str);
+        System.out.println(l);
+    }
+}
