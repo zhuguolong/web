b830bdd7a04dedcb4952bbe053c8447202d5ba58
zhuguolong <zhuguolongi@outlook.com>
2019-02-22 09:53:08 +0800

    Twitter的分布式自增ID雪花算法snowflake (Java版)

--- src/main/java/UUID/Demo_UUID.java
+++ src/main/java/UUID/Demo_UUID.java
@@ -6,5 +6,8 @@ public class Demo_UUID {
     public static void main(String[] args) {
         UUID uuid = UUID.randomUUID();
         System.out.println(uuid);
+
+        String s = UUID.randomUUID().toString();
+        System.out.println(s);
     }
 }
